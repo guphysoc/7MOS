3080583c161d55d9d308b76dc58444f499422dbe
WizardOhio24 <rdmenzies2000@gmail.com>
2020-01-24 08:21:18 +0000

    revealed more speakers

--- script.js
+++ script.js
@@ -5,25 +5,37 @@
 
 talkData = {
     "cir_0_talk": {
-        "speaker": "Dr Richard Middlemiss",
+        "speaker": "Jan 28| Dr Richard Middlemiss",
         "title": "Using tiny gravity sensors to see inside volcanoes",
         "abstract": "In 1985 23,000 people were killed by a volcano in Columbia; this is just one example of the many deadly eruptions that have occurred around the globe in living memory. I’m part of a team that is making sensors that can help to reduce the chance of more deaths like this in the future. We’re creating extremely accurate gravity sensors using the same techniques used to make mobile phone accelerometers.By scanning such sensors over the ground you can infer changes in density, and in doing so create images of magma flow; improving the ability to forecast hazardous events",
         "time_to_be_released": "01/22/2020"
     },
     "cir_1_talk": {
         "speaker": "Dr Daria Sokhan",
-        "title": " Mapping the world within",
+        "title": " Feb 4 | Mapping the world within",
         "abstract": "Protons are a mini-universe in themselves — just like space looks different across different scales (we can see stars, galaxies, the Milky Way as we zoom further and further out), the inner world of the proton appears to change as we zoom further and further in. At a casual glance, it looks like it’s composed of three quarks, but peer deeper inside and it will have shape-shifted into a teeming sea of quarks and gluons. How do its properties come about? What gives it its mass, 1000 times greater than that of its quarks? And what microscopes can we turn towards the infinity of the small at the heart of the proton?",
 
         "time_to_be_released": "01/22/2020"
     },
     "cir_2_talk": {
+        "speaker": "Dr Luke Daly",
+        "title": " Jan 28 | The origin of Earth’s oceans",
+        "abstract": "The Earth’s oceans were critical for the emergence of life. But our cosy nurturing planet was not always so. The Earth formed in a dry rocky region of our Solar System and so was initially a dry barren wasteland, a far cry from the pale blue dot we see today. Where our oceans came from has been a puzzle we’ve been trying to solve for decades. Now, with new analytical technologies, and fresh samples collected from the surface of asteroids we may finally have the missing piece of the puzzle to understand the origin of our blue planet.",
+
         "time_to_be_released": "01/24/2020"
     },
     "cir_3_talk": {
+        "speaker": "Feb 4 | Hannah Doran",
+        "title": "Extraction of heat from radioactive waste",
+        "abstract": "The Nuclear Industry has received a lot of controversy regarding its safety, security and future development after the Fukushima disaster in 2011. Many countries are entering their nuclear decommissioning phase, marking a definitive end to the commercialisation of nuclear energy. However, the build-up of radioactive waste in nuclear stores remains a future burden. My talk will discuss the back-end fuel cycle issues, specifically the interim storage and permanent disposal stages, and suggest novel techniques to recycle and utilise the decay heat produced from radioactive waste for heat and/or electricity production.",
+
         "time_to_be_released": "01/24/2020"
     },
     "cir_4_talk": {
+        "speaker": "Jay Mackenzie",
+        "title": "Feb 4 | On Lines On Boards",
+        "abstract": "From many years of watching lecturers drawing dotted lines on chalk boards, we are motivated to ask \"is there a better way of doing this?\"The answer depends on how your lecturer was drawing their dotted lines, but there is certainly a best* way of drawing them.  In this talk, I motivate our interest in dotted lines, discuss how our preferred line drawing method works, and finish with the underlying mechanical principles that allows efficiency in our dottyness. \n* For a given value of \"best\"",
+
         "time_to_be_released": "01/24/2020"
     },
     "cir_5_talk": {
