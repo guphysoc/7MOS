ac27409af4ae891c8b18c318a0c5cb07f1bdb227
tia <christianemayer5@gmail.com>
2024-01-30 22:55:42 +0000

    updated for 2024

--- script.js
+++ script.js
@@ -5,51 +5,53 @@
 
 talkData = {
     "cir_0_talk": {
-        "speaker": "Dr Richard Middlemiss",
-        "title": "Using tiny gravity sensors to see inside volcanoes",
-        "abstract": "In 1985 23,000 people were killed by a volcano in Columbia; this is just one example of the many deadly eruptions that have occurred around the globe in living memory. I’m part of a team that is making sensors that can help to reduce the chance of more deaths like this in the future. We’re creating extremely accurate gravity sensors using the same techniques used to make mobile phone accelerometers.By scanning such sensors over the ground you can infer changes in density, and in doing so create images of magma flow; improving the ability to forecast hazardous events",
-        "time_to_be_released": "01/22/2020"
+        "speaker": "Kang Xie",
+        "title": "Reconstruct past climate change using the Scoƫsh speleothem record",
+        "abstract": "Speleothems, carbonates grow in the cave, are valuable archives for reconstructing paleoclimate. Unlike the typical speleothems found in carbonate caves, speleothems were recently discovered in a meta-silicate sea cave in Iona, on the west coast of Scotland. Although speleothems have previously been reported from caves in volcanic rocks, speleothems in metamorphic caves have rarely been reported. The Iona speleothems are potentially crucial because paleoclimate reconstructions spanning the Holocene are scarce in Scotland due to a lack of material, particularly speleothems, which can be dated precisely using geochemical dating methods. As for paleoclimate, oxygen isotopes indicate that the amount of precipitation was at a relatively low level between 3000 and 2000 years ago, then increased dramatically from ~2000 to 1760 years ago. These preliminary data indicate that the Iona speleothem has the potential to provide important insights into the climate change.",
+        "time_to_be_released": "01/29/2024"
     },
     "cir_1_talk": {
+        "speaker": "Silvia Renon",
+        "title": "",
+        "abstract": "Drug Coated Balloons (DCB) represent an emerging therapeutic alternative to Drug Eluting  Stents (DES) for the treatment of coronary artery disease. DCB were firstly introduced for  treatment of in-stent restenosis and, due to the promising results achieved, their potential  use has recently been explored in randomized clinical trials for treatment of de novo small vessel lesions and complex lesions (e.g., bifurcations, diffuse disease, long lesions). New  hybrid techniques that require the combined use of DES and DCB are being investigated to  tackle such complex case scenarios where a DES-only approach is often not successful more  effectively. A DCB-only approach is also rising as a possible substitute to DES use in all de  novo lesions cases. DCB present some major advantages when compared to DES, such as  fast and homogeneous drug exchange and the absence of a permanent metallic structure in  the vessel. However, the underlying mechanisms of drug exchange and interactions between  the balloon, polymeric coating, and vessel wall remain incompletely understood. Lesion  preparation is another critical aspect of Percutaneous Coronary Intervention (PCI), consisting  of the pretreatment of the lesion by the means of non-compliant balloons and other  targeted tools to dilate the occluded vessel and facilitate either balloon or stent insertion  and expansion at the site and it is now part of standard clinical practice for PCI. The aim of  this project is to develop an in-silico analysis for PCI, comprehensively modelling lesion  preparation, DCB, and DES insertion and deployment. The focus will be on understanding  the intricate details of these procedures, with an emphasis on studying the effects of lesion  preparation on drug elution and restenosis risk. Additionally, the study seeks to explore the  development of a hybrid surgical procedure that seamlessly integrates both DCBs and DES.  The methodological approach involves the utilization of computational models, finite  element analysis, and structural mechanics. Central to this project is the creation of a  realistic 3D inelastic arterial model, leveraging existing computational frameworks. Many  current models fall short in capturing inelastic behaviour, leading to inaccurate predictions  of DCB and drug elution outcomes. This innovative arterial model will fill this void by  accounting for inelastic deformations, including material failure, plastic strains, and stress  softening effects during balloon or stent deployment. Subsequent simulations will delve into  the influence of balloon expansion on diverse plaque models, varying balloon properties to  discern their impact on drug elution. The outcomes will unravel the complex dynamics  between the balloon, arterial tissue, and drug release, shedding light on critical aspects of  PCI procedures.",
         "time_to_be_released": "01/23/2020"
     },
     "cir_2_talk": {
+        "speaker": "Maria Psarra",
+        "title": "",
+        "abstract": "",
         "time_to_be_released": "01/24/2020"
     },
     "cir_3_talk": {
+        "speaker": "Jaihui Zhao",
+        "title": "Flexoelectricity & Triboelectricity",
+        "abstract": "Triboelectrification, also known as contact electrification, is a widely acknowledged phenomenon, yet the underlying thermodynamic mechanisms remain elusive. In our hypotheses, we posit flexoelectricity as a predominant force driving triboelectrification. This study extensively examines the theories underpinning both flexoelectricity and triboelectricity, accompanied by experimental verification to establish the correlation between these phenomena under specific conditions. The research is bifurcated into two components: one involves simulating the process based on flexoelectricity, while the other entails conducting experiments to substantiate our hypotheses.",
         "time_to_be_released": "01/29/2020"
     },
     "cir_4_talk": {
-        "time_to_be_released": "01/30/2020"
+        "speaker": "Shilong Yan",
+        "title": "",
+        "abstract": "Amidst the energy sector's transition to a 2050 net-zero target, our project in building energy eƯiciency focuses on data quality, sensor optimization, data protocol unification, and AI-model enhancement. We ensure data equality and cross-validation, streamline data migration across protocols, and leverage digital twin technology and innovative sensor networks for precise building energy modelling, aiding people during the energy transition.",
+        "time_to_be_released": "01/29/2020"
     },
     "cir_5_talk": {
-        "time_to_be_released": "01/25/2020"
+        "speaker": "Senthilkumar Subramanian",
+        "title": "Plume-Regolith Interactions in Lunar and Martian Conditions",
+        "abstract": "Given the likelihood of many manned and cargo missions to the Moon and Mars in the next decades, it is crucial to effectively understand the dynamics of particle ejection due to rocket plume interaction during the touchdown phase. Toward this goal, a novel experimental study was conducted in a dedicated state-of-the-art plume-regolith facility housed at the University of Glasgow to track the plume-liberated regolith (dust) in Lunar and Martian environments using particle image velocimetry (PIV) approach. Appropriate regolith simulants were chosen to represent the Lunar and Martian surface soil. A scaled supersonic nozzle represents the lander nozzle with nitrogen as a working gas. The gas properties are adjusted to exhibit realistic nozzle flow physics. A large-volume buffer tank ensures the test is conducted in an extra-terrestrial pressure range. Steady and pulsed jets are examined. Impingement pressure and Infra-Red measurement reveal the plume profile. The PIV vector images show a triangular-shaped sheet of particles sweeping from the regolith bed at high speed at positive inclinations. Close to the nozzle axis, the ejected particles collide with the lander base. Difference in the crater size reveals the influence of cohesive forces among the chosen simulants. Erosion characteristics of simulants are identified. The radio measurement measures the reflectivity, transmissivity, and distortion of the radar signal caused by the interaction between the plume and the regolith in lunar and Martian conditions.",
+        "time_to_be_released": "01/29/2020"
     },
     "cir_6_talk": {
+        "speaker": "Onur Celik",
+        "title": "How can we make solar power farms work after sunset?",
+        "abstract": "The Sun is the closest thing we have to an infinite source of energy. However terrestrial solar power systems can only use solar energy during daylight hours, which limits the effectiveness of solar power to provide clean energy to combat the effects of climate change. What if we had reflectors in space to illuminate solar power farms when direct sunlight is not available to enhance solar power generation? Such orbiting solar reflectors could provide global clean energy in especially the early morning/evening hours, where the clean energy demand may exceed the supply from renewables, which is compensated by fossil fuels. This talk will explain the concept of orbiting solar reflectors and its key aspects in addressing a pressing issue of our society, energy, through a space-based solution.",
         "time_to_be_released": "01/31/2020"
     },
     "cir_7_talk": {
+        "speaker": "Sundas Mulkana",
+        "title": "",
+        "abstract": "The future direction of collaborative robots is shifting from having predefined rules for interaction between human and robot in structured environments, such as industrial settings, to achieving more flexibility in their actions in unstructured environments, such as households and public places. These futuristic robots, trained on learning-based methods, show promising results in simulation and controlled environments. However, when deploying these robots in the real-world ensuring human physical and cognitive safety raises major concerns. This necessitates the development of methods which provide provable safety constraints on robot motion which makes it cognizant of its proximity to humans while performing a task, thus providing formal guarantees that the robot trained on learning-based methods will not come in harmful contact with human. Additionally, studying the effect of these safety constraints on task performance and the ease of collaboration through user feedback in joint action tasks between human and robot would further help improve robot behavior. The primary objectives of this research are to develop methods that prove safety without compromising the performance of the robot, training robot to dynamically react to human behavior in the real world and identify human-preferred robot motions for collaborative tasks. This research's outcomes aim to contribute significantly to the integration of safe collaborative robots in social, healthcare, and industrial environments. By addressing the critical need for safety guarantees and naturalness in human-robot interaction, this work aims to pave the way for safer and more effective human-robot collaboration.",
         "time_to_be_released": "01/26/2020"
     },
-    "cir_8_talk": {
-        "time_to_be_released": "01/27/2020"
-    },
-    "cir_9_talk": {
-        "time_to_be_released": "02/01/2020"
-    },
-    "cir_10_talk": {
-        "time_to_be_released": "01/28/2020"
-    },
-    "cir_11_talk": {
-        "time_to_be_released": "02/02/2020"
-    },
-    "cir_12_talk": {
-        "time_to_be_released": "02/03/2020"
-    },
-    "cir_13_talk": {
-        "time_to_be_released": "02/04/2020"
-    },
-
 }
 
 // Static for the functon below,
@@ -65,11 +67,13 @@ function onClickCircle(id) {
         document.getElementById(prev_id).style.transform = null;
     }
 
+
     // FFade clicked on speaker in
     setTimeout(
         function () {
             document.getElementById("talkAfterClicked").style.opacity = 1;
-            if (Date(talkData[id]["time_to_be_released"]) - Date.now() > 0) {
+            //if (Date(talkData[id]["time_to_be_released"]) - Date.now() > 0) {
+            if (1==1){
                 // The release date has passed, show the user the speaker, title and abstract
                 document.getElementById("talkSpeaker").innerHTML = talkData[id]["speaker"]
                 document.getElementById("talkTitle").innerHTML = talkData[id]["title"]
